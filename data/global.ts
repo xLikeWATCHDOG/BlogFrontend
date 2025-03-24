bff218cefc65af0d37c0a9ae44b6c6602cf5cb01
Birdy <xlikewatchdog@icloud.com>
2025-03-24 12:21:19 +0800

    feat: update BACKEND_URL to use production URL

--- data/global.ts
+++ data/global.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = 'http://localhost:8102';
-// export const BACKEND_URL = 'https://pack2.elfidc.com';
+//export const BACKEND_URL = 'http://localhost:8102';
+export const BACKEND_URL = 'https://pack2.elfidc.com';
